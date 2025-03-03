e7cd30279382ad3479587046d503709fdfbc176b
Ludwig-H <158177790+Ludwig-H@users.noreply.github.com>
2025-03-03 19:23:53 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -121,9 +121,8 @@ class UNET:
     def Drop(self, x):
         #do dropout when self.do_dropout is True
         if self.do_dropout:
-            # Convert x to a Tensor if it's not already
-            x = tf.convert_to_tensor(x) 
-            shape = x.shape.as_list()
+            # shape = x.shape.as_list()
+            shape = list(x.shape)
             noise_shape = [1]*(len(shape)-1)+[shape[-1]]
             x = Dropout(0.5, noise_shape=noise_shape)(x)
         else:
